4b120eec32f604458ec1416b5ff11adef36605c0
chaimsash <me@chaimsash.com>
2017-03-21 14:39:46 -0400

    starting point

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
 // This file should contain all the record creation needed to seed the database with its default values.
-// The data can then be loaded with the node seed.js 
+// The data can then be loaded with the node seed.js
 
 var Promise = require('bluebird');
 var db = require('./models');
